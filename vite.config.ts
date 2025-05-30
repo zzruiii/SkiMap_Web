3b3e2a9d343d3c5ab3e6e4971f5a6b34a352c83e
Rui Zeng <zengrui.jc@gmail.com>
2025-05-30 04:20:25 +0300

    Fix base path and remove duplicate assets for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SkiMap_Web/',
+  base: '/ID_Dev/',
   server: {
     port: 5173,
     host: true
