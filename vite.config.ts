dbdd6237e14ebec780170ecc438502ae7c54cb94
Rui Zeng <zengrui.jc@gmail.com>
2025-05-30 03:26:34 +0300

    Fix base path for SkiMap_Web repository

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ID_Dev/',
+  base: '/SkiMap_Web/',
   server: {
     port: 5173,
     host: true
